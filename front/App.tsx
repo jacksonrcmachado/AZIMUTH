3d8f9d83429da6a0d8e8bf6bec254589e21c2008
cGuilhermec <guilherme-9951@live.com>
2025-06-04 20:10:20 -0300

    feature/alertas deixando eles mais bonitos ao inves de nativos

--- front/App.tsx
+++ front/App.tsx
@@ -9,6 +9,7 @@ import Report from './screens/Report.screen';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
 import Cadastro from './screens/Cadastro.screen';
+import Toast from "react-native-toast-message";
 
 
 const Stack = createNativeStackNavigator();
@@ -17,15 +18,32 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='Initial'>
-          <Stack.Screen name="Login" component={Login} options={{headerShown: false,gestureEnabled: true}} />
+        <Stack.Navigator initialRouteName="Initial">
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false, gestureEnabled: true }}
+          />
           <Stack.Screen name="Initial" component={Inicial} />
-          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
           <Stack.Screen name="Report" component={Report} />
-          <Stack.Screen name="ConfigBuoy" component={ConfigBuoy} options={{ title: 'Configuração do Bóia' }} />
-          <Stack.Screen name="Cadastro" component={Cadastro} options={{ headerShown: false }} />
+          <Stack.Screen
+            name="ConfigBuoy"
+            component={ConfigBuoy}
+            options={{ title: "Configuração do Bóia" }}
+          />
+          <Stack.Screen
+            name="Cadastro"
+            component={Cadastro}
+            options={{ headerShown: false }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
+      <Toast />
     </Provider>
   );
 }
