b469098a2c2f066ac2fde829669015be33607ece
Gustavo Carvalho <gustavocarvalho.gc@gmail.com>
2025-06-10 20:55:16 -0300

    remove btn add buoy

--- front/api/api.ts
+++ front/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-  baseURL: "http://26.115.98.87:3001/graphql",
+  baseURL: "http://192.168.3.32:3001/graphql",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
